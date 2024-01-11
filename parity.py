512080d391f6c1632d4e86c17073057c036b9866
Mohamed Talhaoui <144726758+mohamedtalhaouii@users.noreply.github.com>
2024-01-11 20:36:55 +0100

    Add files via upload

---
+++ parity.py
@@ -0,0 +1,8 @@
+#Parity of number
+N = int(input("Enter a Number :"))
+#paire is Even in English
+if N % 2 == 0:
+    print("This is an even number")
+#impaire is odd in English
+if N % 2 != 0:
+    print("This is an odd number")
